65231a45aac74129a3c50c2a05c6ad3096ac1f31
Todd Smith <todd.smith@firaxis.com>
2020-08-10 10:38:22 -0400

    tmux configs

---
+++ tmux/tmux-BeST-coda-relay.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+SESSIONNAME="BeST-coda-relay"
+tmux has-session -t $SESSIONNAME &> /dev/null
+
+if [ $? != 0 ]
+then
+    tmux new-session -s $SESSIONNAME -d 
+    tmux send-keys 'cd src/projects/tsmith_balw-tsmith3_firaxislive/FiraxisLive/Development/Services/coda-relay' Enter
+    tmux split-window -v
+    tmux send-keys 'cd src/projects/tsmith_balw-tsmith3_firaxislive/FiraxisLive/Development/Services/coda-relay' Enter
+fi
+
+tmux attach -t $SESSIONNAME
---
+++ tmux/tmux-coda-relay.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+SESSIONNAME="coda-relay"
+tmux has-session -t $SESSIONNAME &> /dev/null
+
+if [ $? != 0 ]
+then
+    tmux new-session -s $SESSIONNAME -d 
+    tmux send-keys 'cd src/projects/tsmith_balw-tsmith3_firaxislive/FiraxisLive/Development/Services/coda-relay' Enter
+    tmux split-window -v
+    tmux send-keys 'cd src/projects/tsmith_balw-tsmith3_firaxislive/FiraxisLive/Development/Services/coda-relay' Enter
+    tmux split-window -h htop
+fi
+
+tmux attach -t $SESSIONNAME
